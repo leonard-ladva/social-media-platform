018c6d966e013de7da085fb0a69f1884f908482d
Leonard <leonard.ladva@gmail.com>
2025-11-04 10:47:19 +0200

    Fixed docker setup, before it was using node from outside the container. Also removed unneccesary dockerbuild.sh and removeimages.sh scripts

--- dockerbuild.sh
+++
@@ -1,9 +0,0 @@
-# Build Backend Image
-cd backend
-docker build -t rtf-backend .
-cd ..
-
-# Build Frontend Image
-cd client
-docker build -t rtf-frontend .
-cd ..
--- removeimages.sh
+++
@@ -1,1 +0,0 @@
-docker images rm rtf-backend rtf-frontend
